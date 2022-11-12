b2037732fe6124394cf1d8ada1d773799fbd96b6
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-12 14:56:32 -0600

    created index.vue

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav>
+        <ul class="links">
+            <li><a>Home</a></li>
+            <li><a>Rick</a></li>
+            <li><a>Morty</a></li>
+        </ul>
+    </nav>
+
+</template>
+
+<style scoped>
+.links {
+    display: flex;
+    justify-content: space-evenly;
+}
+</style>
