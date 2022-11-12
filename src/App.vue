63874a20cc264bb71e79da5d0ec13de7db34ca7c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-12 15:26:55 -0600

    added vue-router

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@ import Index from './components/Index.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <Index />
 </template>
 
