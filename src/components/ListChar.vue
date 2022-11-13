9a3e38e78bf8c27e27f8a933ff9682e9fdc46d05
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-12 18:14:30 -0600

    hitting api for ricks

---
+++ src/components/ListChar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    const props = defineProps({
+        charName: String
+    })
+    const res = await fetch(`https://rickandmortyapi.com/api/character/?name=${props.charName}`)
+    const ricks = await res.json()
+    console.log(ricks) 
+</script>
+
+<template>
+    <h1>Ricks output here...</h1>
+    <ul>
+        <li v-for="item in ricks.results">
+            <span v-for="childItem in item.children">
+                {{item.id}} {{childItem}}
+                {{item.name}} {{childItem}}
+            </span>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+</style>
