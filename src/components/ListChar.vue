2e0a966d4441b331239ed0b16c9c45b263379fde
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-12 22:55:39 -0600

    added css

--- src/components/ListChar.vue
+++ src/components/ListChar.vue
@@ -9,17 +9,23 @@
 
 <template>
     <h1>Ricks output here...</h1>
-    <ul>
-        <li v-for="item in ricks.results">
-            <p v-for="(value, key, index) in item">
+    <ul class="info">
+        <li class="data" v-for="item in ricks.results">
+            <div v-for="(value, key, index) in item">
                 #{{index}} [ {{key}}: {{value}} ]
                 <div v-if="index === 8">
                     <img :src="value"/>
                 </div>
-            </p>
+            </div>
         </li>
     </ul>
 </template>
 
 <style scoped>
+.data {
+    margin: 100px;
+    background-color: silver;
+    color: black;
+    border-radius: 16px;
+}
 </style>
