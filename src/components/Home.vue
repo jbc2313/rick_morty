a4f751ac51ba43bdd6c8e06c11de88583f800b34
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-20 22:42:03 -0600

    added imgs of rick and morty on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,20 @@
     <h1>Welcome/Home Page</h1>
     <p>This is the home of all the ricks and mortys that exist.</p>
     <p>Alive or Dead.</p>
+    <div class="imgs">
+        <div>
+            <img src="" alt="rick"/>
+        </div>
+        <div>
+            <img src="" alt="morty"/>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .imgs {
+        display: flex;
+        justify-content: space-evenly;
+    }
 </style>
 
